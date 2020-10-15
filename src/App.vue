3ba8a2590f888cdd58d81962fa99c90b4618f8d2
Todd Peterson <ToddPetersonGV@gmail.com>
2020-10-15 10:37:36 -0600

    Style the vendor login page

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,5 @@ export default class Login extends Vue {}
     font-family: "Nunito Sans", sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    /* text-align: center; */
-    color: #2c3e50;
 }
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,15 @@
 <template>
     <div class="wrap">
         <form class="login" @submit.prevent="login">
-            <h1>Sign in</h1>
+            <h2>Sign in</h2>
             <label>Email</label>
             <input required v-model="email" type="text" />
             <label>Password</label>
             <input required v-model="password" type="password" />
-            <button type="submit">Submit</button>
+            <div class="row button-row">
+                <router-link to="/">Create an account</router-link>
+                <button type="submit">Submit</button>
+            </div>
         </form>
     </div>
 </template>
@@ -35,6 +38,83 @@ export default class Login extends Vue {
 <style scoped>
 .wrap {
     width: 100%;
-    padding-top: 5rem;
+    padding-top: 7rem;
+    display: flex;
+    justify-content: center;
+}
+
+.login {
+    background: #eae2b7;
+    padding: 3rem;
+}
+
+.login h2 {
+    font-weight: 900;
+    font-size: 2em;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+    margin-bottom: 1em;
+}
+
+.login label {
+    display: block;
+    font-weight: 800;
+    text-transform: uppercase;
+    margin-bottom: 0.2em;
+    font-size: 1.1em;
+}
+
+.login input {
+    display: block;
+    width: 400px;
+    height: 40px;
+    border: none;
+    margin-bottom: 1.8rem;
+    border-radius: 6px;
+    box-shadow: none;
+    padding-left: 1em;
+    font-size: 1.1em;
+    color: #2b2b2b;
+}
+
+.login button {
+    font-family: "Nunito Sans", sans-serif;
+    border: none;
+    background: #003049;
+    color: #fcbf49;
+    font-weight: 900;
+    font-size: 1rem;
+    text-transform: uppercase;
+    padding: 0.6rem 1.6rem;
+    border-radius: 6px;
+    cursor: pointer;
+    transition: all 0.3s;
+}
+
+.login button:hover {
+    background: #0f6fa3;
+    color: #f5b556;
+}
+
+.row {
+    width: 100%;
+}
+
+.button-row {
+    display: flex;
+    justify-content: right;
+    align-items: flex-end;
+}
+
+.button-row a,
+.button-row a:visited {
+    margin-right: 2em;
+    color: #003049;
+    transition: color 0.3s;
+}
+
+.button-row a:hover,
+.button-row a:visited:hover {
+    color: #1478ad;
 }
 </style>
